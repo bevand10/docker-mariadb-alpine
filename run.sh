0c69b4c9256cf1b6658fa159b01f00cb6a330419
Dave Bevan <bevand10@yahoo.com>
2017-02-11 18:53:32 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker rm -f mariadb
-docker run -d -v /data/mysql:/data -p 3306:3306 --name mariadb univunix/alpine-mariadb
+docker run -d -v /data/mysql:/data -p 13306:3306 --name mariadb bevand10/alpine-mariadb
